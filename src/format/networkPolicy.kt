162c7e37bf85874e180fa0db9e2acf96843c2ec3
Edan Hamilton <edan.hamilton@uq.edu.au>
2024-08-20 18:44:30 +1000

    Fix: port range of length 1

--- src/format/networkPolicy.kt
+++ src/format/networkPolicy.kt
@@ -8,7 +8,7 @@ import java.io.*
 suspend fun writePolicy() = Templates.ports.copyTo(File("NetworkPolicy.yml"),true).appendWriter().use{file->
 	blockRanges().transform{
 		emit("  - port: ${it.first}")
-		if(it.first!=it.last) emit("   endPort: ${it.last}")
+		if(it.first!=it.last) emit("    endPort: ${it.last}")
 	}.collect(file::appendLine)
 	Templates.ips.reader().close{transferTo(file)}
 	ips().map{"  - ${it.hostAddress}/32"}.collect(file::appendLine)
