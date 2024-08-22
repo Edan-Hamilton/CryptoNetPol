4167d06bb0baf60e73faec1aad606e42b0679c35
Edan Hamilton <edan.hamilton@uq.edu.au>
2024-08-22 16:18:44 +1000

    Fix: etld wildcards

--- src/format/DNSBlock.kt
+++ src/format/DNSBlock.kt
@@ -12,12 +12,14 @@ private fun Trie<Char>.regex(): String = children.map{(k,v)->
 
 private val etlds = runBlocking{
 	getFiles("$gh/publicsuffix/list/master/public_suffix_list.dat")
-		.filterNot{ it.startsWith("//") }.map{it.split('.').asReversed()}.toCollection(Trie())
+		.filterNot{ it.startsWith("//") }
+		.map{it.split('.').asReversed()}
+		.toCollection(Trie())
 }
 
-private val regex = cache
-	.groupBy{it.split('.').run{takeLast(etlds.match(asReversed())+1)}}
-	.map{(tld,doms)->
+private val regex = cache.groupBy{it.split('.').run{takeLast(
+	etlds.match(asReversed().iterator()){if(children.contains("*")) 2 else 1}
+)}}.map{(tld,doms)->
 		if(doms.size==1) doms.single()
 		else tld.joinToString("\\.",".*")
 	}.map{it.asIterable()}.toCollection(Trie()).regex()
--- src/lib/Trie.kt
+++ src/lib/Trie.kt
@@ -2,22 +2,20 @@ package lib
 
 class Trie<T>: AbstractMutableSet<Iterable<T>>() {
 	val children = mutableMapOf<T, Trie<T>>()
-	private var end = false
 
-	override fun add(element: Iterable<T>): Boolean = element
-		.fold(this){node, x->node.children.getOrPut(x){Trie()} }
-			.run{end=true}.let{true}
+	override fun add(element: Iterable<T>): Boolean =
+		element.fold(this){node, x->node.children.getOrPut(x){Trie()} }.let{true}
 
-	fun match(element: Iterable<T>): Int = match(element.iterator())?:0
-	private fun match(el: Iterator<T>): Int? =
-		el.nextOrNull()?.let{children[it]}?.match(el)?.plus(1) ?: if(end) 0 else null
+	fun match(el: Iterator<T>, init: Trie<T>.()->Int): Int =
+		try { children[el.next()]!!.match(el,init) + 1 }
+		catch(_: Exception){ init() }
 
 	override val size by children::size
 
 	override fun iterator() = asSequence().toMutableList().iterator()
 	fun asSequence() = sequence{rec(mutableListOf(),this@Trie)}
 	private suspend fun SequenceScope<List<T>>.rec(prev: MutableList<T>, next: Trie<T>){
-		if(!next.end) for((k,v) in next.children) {
+		if(next.children.isNotEmpty()) for((k,v) in next.children) {
 			prev.add(k)
 			rec(prev,v)
 			prev.removeLast()
