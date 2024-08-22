dcd66b761879f1b216f8b4ba943cd1dba10109a6
Edan Hamilton <edan.hamilton@uq.edu.au>
2024-08-22 16:15:13 +1000

    Fix: Add cryptnono

--- src/format/cryptnono.kt
+++ src/format/cryptnono.kt
@@ -5,8 +5,7 @@ import resources.*
 import java.io.File
 
 suspend fun writeCryptnono(){
-	Templates.cryptnono.copyTo(File("cryptnono.yml"),true)
-		.appendWriter().use{file->
+	Templates.cryptnono.copyTo(File("cryptnono.yml"),true).appendWriter().use{file->
 		algos().toMutableSet().apply{removeAll(cryptnono_allows)}
 			.map(String::asIterable).toCollection(Trie()).asSequence()
 			.map{it.joinToString("","        - ")}
